f45e4552d2a3c17eaca60b60b81f70321392d127
natikebede <71388535+natikebede@users.noreply.github.com>
2021-11-18 15:35:15 +0300

    commite

--- bus-ticketing-system/src/App.js
+++ bus-ticketing-system/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hellow
+          hellow sir ahahah
         </p>
         <a
           className="App-link"
